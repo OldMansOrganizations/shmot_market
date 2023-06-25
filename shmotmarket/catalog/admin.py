d017901e9d662fb1aeafd2f432378bc4d6f49001
CodderLord <verevkin0909@gmail.com>
2023-06-25 15:19:06 +0200

    register persons

---
+++ shmotmarket/catalog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from catalog.models import Products, Categories, SubCategories
+
+
+@admin.register(Products)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Categories)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SubCategories)
+class PersonAdmin(admin.ModelAdmin):
+    pass
