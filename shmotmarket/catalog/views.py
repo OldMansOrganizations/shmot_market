87bb8c84d68d66c5086972b0e51267dffc97b727
CodderLord <verevkin0909@gmail.com>
2023-07-03 15:25:31 +0200

    add categories products

--- shmotmarket/catalog/views.py
+++ shmotmarket/catalog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products, Categories
 # Create your views here.
 
 
 def index(request):
     products = Products.objects.all()
-    context = {'title': 'Главная', 'products': products}
+    categories = Categories.objects.all()
+    context = {'title': 'Главная', 'products': products, 'catalog': categories}
     return render(request, 'catalog/index.html', context=context)
